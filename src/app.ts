f100b11ee7298a08da6d371f6b927bffad739ea4
Renato Benatti <renato.benatti7@gmail.com>
2021-01-05 12:10:44 -0300

    add eslint

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-const express = require('express')
+import express from 'express'
 
 const app = express()
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.send('hello world')
 })
 
